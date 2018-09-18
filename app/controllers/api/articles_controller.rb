f0937c74a39ca7ff577eef7ce602daab51abd34a
Grant Kazan <grantkazan@gmail.com>
2018-09-18 14:17:59 -0500

    rebuilt article data fetch with hardcoded categories

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,7 +1,31 @@
 class Api::ArticlesController < ApplicationController
 
   def index
-    render json: {message: "Hello world!"}
+    @things = [
+      {:category=>"technology"}, 
+      {:category=>"business"}, 
+      {:category=>"sport"}, 
+      {:category=>"weather"}, 
+      {:category=>"music"}, 
+      {:category=>"travel"}
+    ]
+    @things.each do |thing|
+      type = thing[:category]
+      response = Unirest.get(
+        "https://content.guardianapis.com/#{type}?&api-key=#{ENV["API_KEY"]}")
+      @results = response.body["response"]["results"]
+      thing[:data] = @results.map do |result|
+        result["imageUrl"] = nil
+        result
+      end 
+      thing[:currentArticleIndex] = 0
+      thing[:currentArticleVisible] = false
+    end
+    render json: @things
+  end
+
+  def pic
+    render json: {message: "hello!"}
   end
 
 end
